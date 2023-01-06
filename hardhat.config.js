f3fc386d55171b7e5596ce29c3b957dc6056c3a3
web3isthefuture <web3isthefuture@gmail.com>
2023-01-07 00:42:42 +0900

    add hardhat config

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,23 @@
+require("dotenv").config();
+require("@nomiclabs/hardhat-etherscan");
 require("@nomicfoundation/hardhat-toolbox");
+require("@nomicfoundation/hardhat-chai-matchers");
 
-/** @type import('hardhat/config').HardhatUserConfig */
+const PRIVATE_KEY = process.env.PRIVATE_KEY;
+const KLAYTN_TESTNET_RPC_URL = process.env.KLAYTN_TESTNET_RPC_URL;
+
 module.exports = {
   solidity: "0.8.17",
+  defaultNetwork: "klaytn_testnet",
+  networks: {
+    klaytn_testnet: {
+      url: `${KLAYTN_TESTNET_RPC_URL}`,
+      accounts: [`${PRIVATE_KEY}`]
+    },
+  },
+  etherscan: {
+    // Your API key for Etherscan
+    // Obtain one at https://etherscan.io/
+    apiKey: process.env.ETHERSCAN_API_KEY
+  },
 };
